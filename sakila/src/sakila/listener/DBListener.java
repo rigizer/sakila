8c938f0ea2d784f7a37f50f5d5a04be0d90b8e1f
rigizer <rigizer@gmail.com>
2020-10-20 11:22:37 +0900

    Tomcat 실행 시 Class.forName을 호출하는 Listener(DBListener) 제작

---
+++ sakila/src/sakila/listener/DBListener.java
@@ -0,0 +1,24 @@
+package sakila.listener;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class DBListener implements ServletContextListener {
+    // TOMCAT이 켜질 때 실행
+	public void contextInitialized(ServletContextEvent arg0)  { 
+    	System.out.println("DBListener.contextInitialized() 실행");
+    	try {
+    		Class.forName("org.mariadb.jdbc.Driver");
+    		System.out.println("MariaDB 드라이버 로딩");
+    	} catch(ClassNotFoundException e) {
+    		System.out.println("MariaDB 드라이버 로딩 실패");
+    		e.printStackTrace();
+    	}
+    }
+    
+    // TOMCAT이 꺼질 때 실행
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         
+    }
+}
